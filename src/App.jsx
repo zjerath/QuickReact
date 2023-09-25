c173aee0ffdcfa8c19107547d0999ed5eba366d1
Zain Jerath <zainjerath@gmail.com>
2023-09-25 18:04:33 -0500

    Added Style

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css';
 import Banner from './components/Banner';
 import CourseList from './components/CourseList';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const schedule = {
   "title": "CS Courses for 2018-2019",
--- src/components/CourseList.jsx
+++ src/components/CourseList.jsx
@@ -1,7 +1,19 @@
+import '../styles/CourseList.css'
+
 const CourseList = ({courses}) => {
     return (
-        <div>
-            {Object.entries(courses).map(([id, course]) => <div key={id}>{course.term} CS {course.number}: {course.title}</div>)}
+        <div className='course-list m-3'>
+            {Object.entries(courses).map(([id, course]) => 
+                <div className='card m-1 p-2' key={id}>
+                    <div className='card-body'>
+                        <h5 className='card-title'>{course.term} CS {course.number}</h5>
+                        <p className='card-text'>{course.title}</p>
+                    </div>
+                    <div className='card-footer bg-white'>
+                        <p className='card-text'>{course.meets}</p>
+                    </div>
+                </div>
+            )}
         </div>
     )
 };
