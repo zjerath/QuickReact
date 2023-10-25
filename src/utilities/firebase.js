c4a4a5cab616c1a7846919345b47152c3bcbaaf4
Zain Jerath <zainjerath@gmail.com>
2023-10-25 10:15:58 -0500

    local uses emulators

--- src/utilities/firebase.js
+++ src/utilities/firebase.js
@@ -6,7 +6,7 @@ import { getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithPopup, signO
 const firebaseConfig = {
     apiKey: "AIzaSyAobHLEHQ8qhbJQR1NAoatnIUUn-8hk7BI",
     authDomain: "quickreact-ea95e.firebaseapp.com",
-    databaseURL: "https://quickreact-ea95e-default-rtdb.firebaseio.com",
+    databaseURL: "https://quickreact-ea95e.firebaseio.com",
     projectId: "quickreact-ea95e",
     storageBucket: "quickreact-ea95e.appspot.com",
     messagingSenderId: "531452138536",
